00c018743fe31cc1880f3591606c566d96d24677
Gabriel Lima <gabriellima0409@gmail.com>
2021-02-07 15:21:47 -0300

    variáveis

---
+++ JavaScript Completo ES6/variaveis/script.js
@@ -0,0 +1,71 @@
+var nome = 'Gabriel';
+var idade = 28;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 20;
+var totalComprado = 5;
+var precoTotal = preco * totalComprado;
+
+console.log(precoTotal);
+
+var sobrenome = 'Lima', 
+    cidade = 'Fortaleza';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefine);
+
+// Hoisting
+var comida;
+console.log(comida);
+comida = 'Pizza';
+console.log(comida);
+
+let time = 'Vasco';
+// let time = 'Vasco'; // Retorna um erro
+
+/*
+var time = 'Santos';
+var time = 'Ceará';  Não retorna erro 
+*/
+
+time = 'Flamengo';
+
+console.log(time);
+
+/*============================================
+    EXERCÍCIO
+// Declarar uma variável com o seu nome
+var nome = 'Gabriel';
+
+// Declarar uma variável com a sua idade
+var idade = 21;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+// Atribuir valor a sua comida favorita
+var comidaFavorita;
+comidaFavorita = 'pizza';
+
+// Delcarar 5 variáveis diferentes sem valores
+var numero = '255'
+    bairro,
+    cidade,
+    estado,
+    rua,
+
+console.log(nome, idade, comidaFavorita)
+============================================*/
+
+
+
+
+
+
+
+
